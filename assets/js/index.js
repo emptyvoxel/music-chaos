88683c5910e6a469aead5ebeb201299679b28fca
emptyvoxel <89980949+emptyvoxel@users.noreply.github.com>
2021-10-11 03:05:48 -0300

    Add audio analyser

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,33 @@
 const SCALE = 10;
-const PARTICLES = 100;
+const PARTICLES = 500;
 const CHUNKS = Math.floor(innerHeight / SCALE);
 const flowField = new Array(CHUNKS * CHUNKS);
-
 const particles = [];
+
+const audio = new Audio();
+const audioContext = new AudioContext();
+const audioInput = document.createElement('input');
+audioInput.type = 'file';
+
+const track = audioContext.createMediaElementSource(audio);
+const analyser = audioContext.createAnalyser();
+analyser.fftSize = Math.pow(2, 14);
+
+track.connect(analyser);
+track.connect(audioContext.destination);
+
+let dataArray = new Uint8Array(analyser.frequencyBinCount);
+
+audioInput.oninput = ({ target }) => {
+    audio.setAttribute('src', URL.createObjectURL(target.files[0]));
+    audioContext.resume();
+    audio.play();
+    loop();
+}
+
+audio.onended = () => {
+    noLoop();
+}
 
 function setup () {
     createCanvas(innerHeight, innerHeight);
@@ -13,15 +37,21 @@ function setup () {
         particles.push(new Particle());
     }
 
+    stroke(255, 255, 255, 25);
     background(0);
-    stroke(0, 255, 255, 30);
+    noLoop();
+
+    document.body.appendChild(audioInput);
 }
 
 function draw () {
+    analyser.getByteFrequencyData(dataArray);
     for (let i = 0; i < CHUNKS; i++) {
         for (let j = 0; j < CHUNKS; j++) {
             const index = j + i * CHUNKS;
-            const angle = random(10);
+            // No idea of what this does...
+            // I tested a bunch of stuff and this one was the best...
+            const angle = (dataArray[j] / 128) + (dataArray[i+j] / 128) * PI;
             
             const vector = p5.Vector.fromAngle(angle);
             flowField[index] = vector;
