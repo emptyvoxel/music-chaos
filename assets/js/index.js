d8319822b686656afb9b4689cb7db2e1d8a9da2e
emptyvoxel <89980949+emptyvoxel@users.noreply.github.com>
2021-10-11 01:33:44 -0300

    Add particles

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,37 @@
+const SCALE = 10;
+const PARTICLES = 100;
+const CHUNKS = Math.floor(innerHeight / SCALE);
+const flowField = new Array(CHUNKS * CHUNKS);
+
+const particles = [];
+
 function setup () {
     createCanvas(innerHeight, innerHeight);
     colorMode(RGB, 255);
 
+    for (let i = 0; i < PARTICLES; i++) {
+        particles.push(new Particle());
+    }
+
     background(0);
-    stroke(255, 255, 255, 30);
+    stroke(0, 255, 255, 30);
 }
 
 function draw () {
+    for (let i = 0; i < CHUNKS; i++) {
+        for (let j = 0; j < CHUNKS; j++) {
+            const index = j + i * CHUNKS;
+            const angle = random(10);
+            
+            const vector = p5.Vector.fromAngle(angle);
+            flowField[index] = vector;
+        }
+    }
+
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].move(flowField);
+        particles[i].update();
+        particles[i].edges();
+        particles[i].render();
+    }
 }
---
+++ assets/js/particle.js
@@ -0,0 +1,66 @@
+// I took this code from a Perlin Noise project I did a few years ago
+// When I first wrote it, I was a following a tutorial/challenge by
+// The Coding Train: https://www.youtube.com/watch?v=BjoM9oKOAKY
+
+class Particle {
+    constructor (x=random(innerHeight), y=random(innerHeight)) {
+        this.position = createVector(x, y);
+        this.before = this.position.copy();
+        this.velocity = createVector(0, 0);
+        this.acceleration = createVector(0, 0);
+        this.maxSpeed = 5;
+    }
+
+    update () {
+        this.velocity.add(this.acceleration);
+        this.velocity.limit(this.maxSpeed);
+        this.position.add(this.velocity);
+        this.acceleration.mult(0);
+    }
+
+    move (vectors) {
+        const x = Math.floor(this.position.x / SCALE);
+        const y = Math.floor(this.position.y / SCALE);
+        const index = x + y * CHUNKS;
+
+        this.acceleration.add(vectors[index]);
+    }
+
+    updateBefore () {
+        this.before.x = this.position.x;
+        this.before.y = this.position.y;
+    }
+
+    edges () {
+        if (this.position.x > width) {
+            this.position.x = 0;
+            this.updateBefore();
+        }
+
+        else if (this.position.x < 0) {
+            this.position.x = width;
+            this.updateBefore();
+        }
+
+        if (this.position.y > height) {
+            this.position.y = 0;
+            this.updateBefore();
+        }
+
+        else if (this.position.y < 0) {
+            this.position.y = height;
+            this.updateBefore();
+        }
+    }
+
+    render () {
+        line(
+            this.position.x,
+            this.position.y,
+            this.before.x,
+            this.before.y
+        );
+
+        this.updateBefore();
+    }
+}
